2b51ceb6e5b974899381f1dc3c2e71f1b048b565
Mois-hl <moisesae182@gmail.com>
2022-09-14 20:07:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'
+import Navbar from './components/navbar/navbar'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <Navbar />
+      <header className={styles.header} >
+        <div className={styles.title} >
+          <h1>3er COLOQUIO DE CÓMPUTO CUÁNTICO DEL CIC</h1>
+          <p>Organizado por los laboratorios de Cómputo no Convencional y el de Simulacion y Modelado</p>
+        </div>
       </header>
+      <div className={styles.mainContainer}>
+        <div className={styles.container}>
+          <div>
+            <h2>Acerca de</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore!</p>
+          </div>
+          <img className={styles.images} src={require('./assets/FallFestWordMark.png')} alt='logo evento' />
+        </div>
+        <div className={styles.container}>
+          <div>
+            <h2>Fechas</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore!</p>
+          </div>
+          <div style={{background: '#00000055', width:'100%', height:'20rem', borderRadius: 20}}></div>
+        </div>
+        <div className={styles.container}>
+          <div>
+            <h2>Expositores</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore!</p>
+          </div>
+          <div style={{background: '#00000055', width:'100%', height:'20rem', borderRadius: 20}}></div>
+        </div>
+        <div className={styles.container}>
+          <div>
+            <h2>Hackathon</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore!</p>
+          </div>
+          <div style={{background: '#00000055', width:'100%', height:'20rem', borderRadius: 20}}></div>
+        </div>
+        <div className={styles.prueba}></div>
+        <div className={styles.container}>
+          <div>
+            <h2>Hackathon</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias reprehenderit a doloremque saepe exercitationem ullam corrupti commodi id perspiciatis magni molestias, facilis possimus iusto earum modi sunt magnam unde voluptatum voluptate quod dignissimos optio, officiis laborum nulla. Et, cum labore!</p>
+          </div>
+          <div style={{width:'100%', height:'100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 10}}>
+            <div style={{background: '#00000055', width:'100%', height:'40%', borderRadius: 20}}></div>
+              <p>Para registrarse de click en el botón</p>
+              <a href='https://airtable.com/shrUZ218gQFxLE1ML' className={styles.boton}>Registro</a >
+          </div>
+        </div>
+        <div className={styles.containerSecondary}>
+          <h2>Organizadores</h2>
+          <div className={styles.organizadores}>
+            <ul>
+              <li style={{ listStyle: 'none', marginBottom: 10 }}><strong>Presidente: </strong></li>
+              <li>M. en C. Alberto Maldonado Romo</li>
+            </ul>
+            <ul>
+              <li style={{ listStyle: 'none', marginBottom: 10 }}><strong>Comité técnico: </strong></li>
+              <li>Dr. Jesús Yaljá Montiel Pérez</li>
+              <li>Dr. Juan Humberto Sossa Azuela</li>
+              <li>Dr. Ponciano Jorge Escamilla Ambrosio</li>
+              <li>Dra. Elsa Rubio Espino</li>
+              <li>Dr. Erik Zamora Gómez</li>
+              <li>Dr. Sergio Vidan Beltran</li>
+            </ul>
+            <ul>
+              <li style={{ listStyle: 'none', marginBottom: 10 }}><strong>Coordinador adm.: </strong></li>
+              <li>Dr. Juan Carlos Chimal Eguia</li>
+            </ul>
+            <ul>
+              <li style={{ listStyle: 'none', marginBottom: 10 }}><strong>Apoyo logístico: </strong></li>
+              <li>M en C. Elizabeth López Lozada</li>
+              <li>M en C. David Peréz Martínez</li>
+              <li>M en C. José Angel Martínez Navarro</li>
+              <li>M en C. Mario Alfredo Ibarra Carrillo</li>
+              <li>Ing. Diana Patricia Barragán Vázquez</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <footer className={ styles.footer}>
+        <div style={{ width: 300 }}>
+          <img src={require('./assets/logoLab.png')} alt='logo cic' />
+        </div>
+        <div>
+          <p>Instituto Politécnico Nacional</p>
+          <p>Centro de Investigación en Computación</p>
+          <p>Laboratorio de Robótica y Mecatrónica</p>
+        </div>
+        <div>
+          <p><strong>Dirección</strong></p>
+          <p>Av. Juan de Dios Bátiz, esq. Miguel Othón de Mendizábal,</p>
+          <p>Col.Nueva Industrial Vallejo, Alcaldía Gustavo A. Madero,<br></br> C.P. 07738, CDMX</p>
+        </div>
+      </footer>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import styles from './navbar.module.css'
+
+export default function Navbar () {
+  return (
+    <nav className={styles.container}>
+      <img src={require('../../assets/RyM.png')} alt='logo cic' />
+      <ul className={styles.menu}>
+        <li><a href='/#'>Inicio</a></li>
+        <li><a href='/#'>Acerca de</a></li>
+        <li><a href='/#'>Fechas</a></li>
+        <li><a href='/#'>Expositores</a></li>
+        <li><a href='/#'>Hackathon</a></li>
+        <li><a href='/#'>Registro</a></li>
+        <li><a href='/#'>Organizadores</a></li>
+      </ul>
+    </nav>
+  )
+}
